bfc7093a3fa69d4d37dbbcf61789b62dfd62ad3b
dipeshghimire1997 <you@example.com>
2020-05-04 16:53:03 +0545

    dipesh

--- schoole_front/src/Hoc/Layout.js
+++ schoole_front/src/Hoc/Layout.js
@@ -1,13 +1,17 @@
 import React from "react";
 import Home from "../Components/Home/Home";
 import About from "../Components/About/About";
+import Application from "../Components/Application/Application";
 
 export default function Layout() {
   return (
     <div>
       <div>First ma Nav bar </div>
+  
 
       <div>
+        
+        <Application />
         <Home />
         <About />
       </div>
