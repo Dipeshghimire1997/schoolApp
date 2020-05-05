d784e05a0cafb9db0f84f1a9463bd5636c47d69a
Orish <hsirov321@gmail.com>
2020-05-05 19:22:09 +0545

    final view adminision

--- schoole_front/src/Components/Adminission/Adminission.js
+++ schoole_front/src/Components/Adminission/Adminission.js
@@ -29,168 +29,176 @@ let Adminission = (props) => {
 
       {/* form start */}
       <form>
-        <div className="col">
-          {/* 0 */}
-          <div className="g-row">
-            <div>
-              <label>Class Applied For :</label>{" "}
-              <input name="classApplied" type="text" />
-            </div>
-            <div>
-              <label>S.No :</label>
-              <input name="s.no" type="number" />
-            </div>
-          </div>
-          {/* 1 */}
-          <div className="g-row">
-            <div>
-              <label>Name of Pupil</label>
-              <input name="namePupil" type="text" />
-            </div>
-            <div className="row space-around">
-              <div>
-                <label>Girl : </label>
-                <input name="gender" type="radio" value="girl" />
-              </div>
-              <div>
-                <label>Boy : </label>
-                <input name="gender" type="radio" value="boy" />
-              </div>
-              <div>
-                <label>Other : </label>
-                <input name="gender" type="radio" value="other" />
-              </div>
-            </div>
-          </div>
-          {/* 2 */}
-          <div className="g-row">
-            <div>
-              <label>Date Of Birth</label>
-              <input name="dob" type="date" />
-            </div>
-          </div>
-          {/* 3 */}
-          <div className="g-row">
-            <div>
-              <label>Place Of Birth : </label>
-              <input name="placedob" type="text" />
-            </div>
-            <div>
-              <label>Nationality : </label>
-              <input name="nationality" type="text" />
-            </div>
-          </div>
-
-          {/* 5 */}
-          <div className="g-row">
-            <div>
-              <label>Religion : </label>
-              <input name="religion" type="text" />
-            </div>
-          </div>
-
-          {/* 6 */}
-          <div className="g-row ">
-            <div className="col border-all">
-              <span style={{ alignSelf: "center " }}> Father</span>
-              <div>
-                <label>Name : </label>
-                <input name="fName" type="text" />
-              </div>
-              <div>
-                <label>Occupation : </label>
-                <input name="fOccupation" type="text" />
-              </div>
-              <div>
-                <label>Mobole No : </label>
-                <input name="fMoboleNo" type="text" />
-              </div>
-              <div>
-                <label>Off. No : </label>
-                <input name="fOffNo" type="text" />
-              </div>
-            </div>
-            <div className="col border-all">
-              <span style={{ alignSelf: "center " }}> Mother</span>
-              <div>
-                <label>Name : </label>
-                <input name="mName" type="text" />
-              </div>
-              <div>
-                <label>Occupation : </label>
-                <input name="mOccupation" type="text" />
-              </div>
-              <div>
-                <label>Mobole No : </label>
-                <input name="mMoboleNo" type="text" />
-              </div>
-              <div>
-                <label>Off. No : </label>
-                <input name="mOffNo" type="text" />
-              </div>
-            </div>
-          </div>
-          {/* form end */}
-
-          {/* 7 */}
-          <div className="g-row">
-            <div>
-              <label>Full Address :</label>
-              <input type="text" name="pFAddress" />
-            </div>
-          </div>
-          <div>
-            <label>Details Of Guardian</label>
+        <div className="border-all">
+          <div className="col" style={{ marginTop: "1rem" }}>
+            {/* 0 */}
             <div className="g-row">
               <div>
-                <label>Name : </label>
-                <input name="gName" type="text" />
+                <label>Class Applied For :</label>{" "}
+                <input name="classApplied" type="text" />
               </div>
               <div>
-                <label>Full Address : </label>
-                <input name="fAddress" type="text" />
-              </div>
-              <div>
-                <label>Contact Nos :</label>
-                <input name="allContactNo" type="text" />
+                <label>S.No :</label>
+                <input name="s.no" type="number" />
               </div>
             </div>
-          </div>
-
-          {/* 8 */}
-          <div>
-            <label>Medical History Of The Student</label>
+            {/* 1 */}
             <div className="g-row">
               <div>
-                <label>Blood Group :</label>
-                <input name="bloodGroup" type="text" />
+                <label>Name of Pupil</label>
+                <input name="namePupil" type="text" />
+              </div>
+              <div className="row space-around">
+                <div>
+                  <label>Girl : </label>
+                  <input name="gender" type="radio" value="girl" />
+                </div>
+                <div>
+                  <label>Boy : </label>
+                  <input name="gender" type="radio" value="boy" />
+                </div>
+                <div>
+                  <label>Other : </label>
+                  <input name="gender" type="radio" value="other" />
+                </div>
+              </div>
+            </div>
+            {/* 2 */}
+            <div className="g-row">
+              <div>
+                <label>Date Of Birth</label>
+                <input name="dob" type="date" />
+              </div>
+            </div>
+            {/* 3 */}
+            <div className="g-row">
+              <div>
+                <label>Place Of Birth : </label>
+                <input name="placedob" type="text" />
               </div>
               <div>
-                <label>Vaccinations :</label>
-                <input name="vaccinationsTaken" type="text" />
+                <label>Nationality : </label>
+                <input name="nationality" type="text" />
               </div>
+            </div>
+
+            {/* 5 */}
+            <div className="g-row">
               <div>
-                <label>Any Allergies:</label>
-                <input name="anyAllergies" type="text" />
+                <label>Religion : </label>
+                <input name="religion" type="text" />
+              </div>
+            </div>
+
+            {/* 6 */}
+            <div className="g-row ">
+              <div className="col border-all">
+                <span style={{ alignSelf: "center " }}> Father</span>
+                <div>
+                  <label>Name : </label>
+                  <input name="fName" type="text" />
+                </div>
+                <div>
+                  <label>Occupation : </label>
+                  <input name="fOccupation" type="text" />
+                </div>
+                <div>
+                  <label>Mobole No : </label>
+                  <input name="fMoboleNo" type="text" />
+                </div>
+                <div>
+                  <label>Off. No : </label>
+                  <input name="fOffNo" type="text" />
+                </div>
+              </div>
+              <div className="col border-all">
+                <span style={{ alignSelf: "center " }}> Mother</span>
+                <div>
+                  <label>Name : </label>
+                  <input name="mName" type="text" />
+                </div>
+                <div>
+                  <label>Occupation : </label>
+                  <input name="mOccupation" type="text" />
+                </div>
+                <div>
+                  <label>Mobole No : </label>
+                  <input name="mMoboleNo" type="text" />
+                </div>
+                <div>
+                  <label>Off. No : </label>
+                  <input name="mOffNo" type="text" />
+                </div>
+              </div>
+            </div>
+            {/* form end */}
+
+            {/* 7 */}
+            <div className="g-row" style={{ marginTop: "1rem" }}>
+              <div>
+                <label>Full Address :</label>
+                <input type="text" name="pFAddress" />
+              </div>
+            </div>
+            <div>
+              <label>Details Of Guardian</label>
+              <div className="g-row">
+                <div>
+                  <label>Name : </label>
+                  <input name="gName" type="text" />
+                </div>
+                <div>
+                  <label>Full Address : </label>
+                  <input name="fAddress" type="text" />
+                </div>
+                <div>
+                  <label>Contact Nos :</label>
+                  <input name="allContactNo" type="text" />
+                </div>
+              </div>
+            </div>
+
+            {/* 8 */}
+            <div>
+              <label>Medical History Of The Student</label>
+              <div className="g-row">
+                <div>
+                  <label>Blood Group :</label>
+                  <input name="bloodGroup" type="text" />
+                </div>
+                <div>
+                  <label>Vaccinations :</label>
+                  <input name="vaccinationsTaken" type="text" />
+                </div>
+                <div>
+                  <label>Any Allergies:</label>
+                  <input name="anyAllergies" type="text" />
+                </div>
               </div>
             </div>
           </div>
         </div>
-        <div>
-          <div>
+
+        <div className=" border-all">
+          <div
+            className="col"
+            style={{ marginTop: "1rem", marginBottom: "1rem" }}
+          >
             <div>
-              <label>Place :</label>
-              <input type="text" name="place" />
-            </div>
-            <div>
-              <input type="checkbox" />
-              <label>
-                I declare that the details mentioned above about my child are
-                true . I shall abide by the rule & refulations of the schoole
-              </label>
-            </div>
-            <div>
-              <button>Submit</button>
+              <div>
+                <label>Place :</label>
+                <input type="text" name="place" />
+              </div>
+              <div>
+                <input type="checkbox" />
+                <label>
+                  I declare that the details mentioned above about my child are
+                  true . I shall abide by the rule & refulations of the schoole
+                </label>
+              </div>
+              <div>
+                <button>Submit</button>
+              </div>
             </div>
           </div>
         </div>
