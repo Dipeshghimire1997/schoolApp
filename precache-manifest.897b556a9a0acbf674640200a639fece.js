4175093864058fe231981b7b526c12e61d8a9c0c
Orish <hsirov321@gmail.com>
2020-05-05 22:09:05 +0545

    Updates

---
+++ precache-manifest.897b556a9a0acbf674640200a639fece.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b964cdc4b9ae3ecfea58cf2873284ee0",
+    "url": "/schoolApp/index.html"
+  },
+  {
+    "revision": "a2f13e099bd7339a84ac",
+    "url": "/schoolApp/static/css/2.e3885d78.chunk.css"
+  },
+  {
+    "revision": "913011af0e64e8a24dbf",
+    "url": "/schoolApp/static/css/main.a9f2d649.chunk.css"
+  },
+  {
+    "revision": "a2f13e099bd7339a84ac",
+    "url": "/schoolApp/static/js/2.ebca1287.chunk.js"
+  },
+  {
+    "revision": "4fdb92f5a0a11be5d79ae606cb253c2b",
+    "url": "/schoolApp/static/js/2.ebca1287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "913011af0e64e8a24dbf",
+    "url": "/schoolApp/static/js/main.232986fb.chunk.js"
+  },
+  {
+    "revision": "7ba66881169793fc22e9",
+    "url": "/schoolApp/static/js/runtime-main.1834606d.js"
+  },
+  {
+    "revision": "10e069e5c466669e49694bc604e7ef78",
+    "url": "/schoolApp/static/media/c1.10e069e5.jpg"
+  },
+  {
+    "revision": "302d2cdf61a4d293a17b08b0c825514f",
+    "url": "/schoolApp/static/media/c3.302d2cdf.jpg"
+  },
+  {
+    "revision": "14f219b744cc2ec5ffb0dde05cc2bec6",
+    "url": "/schoolApp/static/media/c5.14f219b7.jpg"
+  },
+  {
+    "revision": "16114559cb3c4e1b3f1d6686cd520320",
+    "url": "/schoolApp/static/media/c6.16114559.jpg"
+  },
+  {
+    "revision": "ae11993a5a2d0b6442f85477cd32e03e",
+    "url": "/schoolApp/static/media/c7.ae11993a.jpg"
+  },
+  {
+    "revision": "39379b53fa6224d0dda42c0cf17b621c",
+    "url": "/schoolApp/static/media/undraw_directions_x53j.39379b53.svg"
+  }
+]);
