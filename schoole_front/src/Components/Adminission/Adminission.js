05b86b320bff1de915f1a04e7e4a4a2e55f58ccb
Orish <hsirov321@gmail.com>
2020-05-05 19:14:13 +0545

    form complete

--- schoole_front/src/Components/Adminission/Adminission.js
+++ schoole_front/src/Components/Adminission/Adminission.js
@@ -1,180 +1,210 @@
 import React from "react";
 
-export default function Adminission() {
+let Adminission = (props) => {
   return (
-    <div>
-      <div className="col">
-        <div className="row space-around center">
-          <div>CHAMPS WORLD</div>
-          <div className="col center ">
-            <h1>CHAMPS WORLD</h1>
-            <div>Dallu , Chagal,Kathmandu</div>
-            <div>Call :9849180494 , 9808581308</div>
-            <div>FaceBook :WWW.Facebook.com/champsworld</div>
+    <div className="application-form-page ">
+      {/* the header  */}
+      <div className="row space-around">
+        <div>Logo</div>
+        <div className="center">
+          <h1>CHAMPS WORLD</h1>
+          <div>Dallu , Chagal , Kathmandu</div>
+          <div>9849180494/9808581308</div>
+          <div>
+            FaceBook ID :
+            <a href="https://www.facebook.com/champswordl">
+              facebook.com/champswordl
+            </a>
           </div>
-          <div>picture</div>
         </div>
+        <div>Image</div>
+      </div>
+      {/* appolcation info */}
+      <div className="m-3r">
+        <center>
+          <h1>APPLICANT FORM</h1>
+          To Be Filled by Parent or Guardian In Capital Letters Only
+        </center>
       </div>
 
-      <span> To Be Filled By Parent or Gardian In Capital Letter Only</span>
-      <div className="container">
-        <br />
-        <form>
-
-
-
-          <div className="row">
-            <lable>1.Class Applied Form :</lable>
-
-            <input type="text" name="clsAppliedForm" />
+      {/* form start */}
+      <form>
+        <div className="col">
+          {/* 0 */}
+          <div className="g-row">
             <div>
-
-              <label>2.S.no: </label> <input type="text" name="sno" />
-            </div>
-          </div>
-
-
-          <div>
-            <label>3.Name of Pupil :  </label>
-            <input name="pupilName" type="text" />
-          </div>
-
-          <div className="row" >
-
-            <label>4.Gender:</label>
-
-
-
-            <label>GIRL:</label><input type="radio" name="gender" value="girl" />
-
-
-
-            <label>BOY:</label>
-
-
-            <input type="radio" name="gender" value="boy" />
-
-            <label>OTHER:</label>
-            <input type="radio" name="gender" value="other" />
-
-
-
-
-          </div>
-
-
-
-
-          <div className="row" >
-
-
-            <label>6.Place of Birth : </label>
-            <input name="placeOfBirth" type="text" />
-
-
-            <div>
-              <label>5.Date of Birth : </label> <input name="dob" type="date" />
-            </div>
-
-          </div>
-          <div>
-            <label>7.Nationality : </label>
-            <input name="nationality" type="text" />
-          </div>
-
-
-          <div>
-            <label>8.Religion: </label>
-            <input name="religion" type="text" />
-          </div>
-
-          <div>
-            <fieldset>
-              <legend>9.DETAILS OF PARENTS:</legend>
-
-              <table border="2px, solid ">
-                <tr align="center">
-                  <th>FATHER</th>
-                  <th>MOTHER</th>
-                </tr>
-                <tr>
-                  <td><label>Name: </label>
-                    <input name="fName" type="text" /> </td>
-                  <td><label>Name: </label>
-                    <input name="fName" type="text" /> </td>
-                </tr>
-                <tr>
-                  <td><label>OCCUPATION: </label>
-                    <input name="occupation" type="text" /> </td>
-                  <td><label> OCCUPATION:</label>
-                    <input name="occupation" type="text" /> </td>
-                </tr>
-                <tr>
-                  <td><label>Mobile No.: </label>
-                    <input name="mobileno" type="text" /></td>
-                  <td><label>Mobile No.: </label>
-                    <input name="mobileno" type="text" /></td>
-                </tr>
-                <tr>
-                  <td><label>Office Phone: </label>
-                    <input name="oPhone" type="text" /></td>
-                  <td><label>Office Phone: </label>
-                    <input name="oPhone" type="text" /></td>
-                </tr>
-              </table>
-            </fieldset>
-          </div><br />
-          <div>
-            <label>10.FULL ADDRESH: </label>
-            <input name="fullAddresh" type="text" placeholder="FULL ADDRESH " />
-          </div>
-          <div>
-            <label>11.DETAILS OF GUARDIAN:</label>
-            <input name="details" type="text" placeholder="NAME" /><br />
-            <label>12.FULL ADDRESH: </label>
-            <input name="fullAddresh" type="text" placeholder="FULL ADDRESH " /><br />
-            <label>13.CONTACT Nos.: </label>
-            <input name="contactnos" type="text" placeholder="CONTACT NOS " /><br />
-          </div>
-          <div>
-            <div>
-              <label>14.BLOOD GROUP: </label>
-              <input name="bloodGroup" type="text" />
+              <label>Class Applied For :</label>{" "}
+              <input name="classApplied" type="text" />
             </div>
             <div>
-              <label>15.VACCINATIONS TAKEN: </label>
-              <input name="vaccinationTaken" type="text" />
+              <label>S.No :</label>
+              <input name="s.no" type="number" />
             </div>
           </div>
-          <div>
-            <label>16.ANY ALLERGIES: </label>
-            <input name="anyAllergies" type="text" />
-          </div>
-          <hr /><input type="checkbox" />
-          <p>I Declare the details mention above my child are true.I Shall abide by the rule and regulation of this school. </p>
-          <br />
-
-          <div className="row">
-            <lable>17.PLACE:</lable>
-
-            <input type="text" name="place" />
+          {/* 1 */}
+          <div className="g-row">
             <div>
-              <label>18 DATE: </label> 
+              <label>Name of Pupil</label>
+              <input name="namePupil" type="text" />
+            </div>
+            <div className="row space-around">
+              <div>
+                <label>Girl : </label>
+                <input name="gender" type="radio" value="girl" />
+              </div>
+              <div>
+                <label>Boy : </label>
+                <input name="gender" type="radio" value="boy" />
+              </div>
+              <div>
+                <label>Other : </label>
+                <input name="gender" type="radio" value="other" />
+              </div>
+            </div>
+          </div>
+          {/* 2 */}
+          <div className="g-row">
+            <div>
+              <label>Date Of Birth</label>
               <input name="dob" type="date" />
             </div>
           </div>
+          {/* 3 */}
+          <div className="g-row">
+            <div>
+              <label>Place Of Birth : </label>
+              <input name="placedob" type="text" />
+            </div>
+            <div>
+              <label>Nationality : </label>
+              <input name="nationality" type="text" />
+            </div>
+          </div>
 
+          {/* 5 */}
+          <div className="g-row">
+            <div>
+              <label>Religion : </label>
+              <input name="religion" type="text" />
+            </div>
+          </div>
 
+          {/* 6 */}
+          <div className="g-row ">
+            <div className="col border-all">
+              <span style={{ alignSelf: "center " }}> Father</span>
+              <div>
+                <label>Name : </label>
+                <input name="fName" type="text" />
+              </div>
+              <div>
+                <label>Occupation : </label>
+                <input name="fOccupation" type="text" />
+              </div>
+              <div>
+                <label>Mobole No : </label>
+                <input name="fMoboleNo" type="text" />
+              </div>
+              <div>
+                <label>Off. No : </label>
+                <input name="fOffNo" type="text" />
+              </div>
+            </div>
+            <div className="col border-all">
+              <span style={{ alignSelf: "center " }}> Mother</span>
+              <div>
+                <label>Name : </label>
+                <input name="mName" type="text" />
+              </div>
+              <div>
+                <label>Occupation : </label>
+                <input name="mOccupation" type="text" />
+              </div>
+              <div>
+                <label>Mobole No : </label>
+                <input name="mMoboleNo" type="text" />
+              </div>
+              <div>
+                <label>Off. No : </label>
+                <input name="mOffNo" type="text" />
+              </div>
+            </div>
+          </div>
+          {/* form end */}
 
+          {/* 7 */}
+          <div className="g-row">
+            <div>
+              <label>Full Address :</label>
+              <input type="text" name="pFAddress" />
+            </div>
+          </div>
+          <div>
+            <label>Details Of Guardian</label>
+            <div className="g-row">
+              <div>
+                <label>Name : </label>
+                <input name="gName" type="text" />
+              </div>
+              <div>
+                <label>Full Address : </label>
+                <input name="fAddress" type="text" />
+              </div>
+              <div>
+                <label>Contact Nos :</label>
+                <input name="allContactNo" type="text" />
+              </div>
+            </div>
+          </div>
 
-        </form>
-        <div>
-          <input type="submit" value="Submit"></input>
+          {/* 8 */}
+          <div>
+            <label>Medical History Of The Student</label>
+            <div className="g-row">
+              <div>
+                <label>Blood Group :</label>
+                <input name="bloodGroup" type="text" />
+              </div>
+              <div>
+                <label>Vaccinations :</label>
+                <input name="vaccinationsTaken" type="text" />
+              </div>
+              <div>
+                <label>Any Allergies:</label>
+                <input name="anyAllergies" type="text" />
+              </div>
+            </div>
+          </div>
         </div>
-
-      </div>
+        <div>
+          <div>
+            <div>
+              <label>Place :</label>
+              <input type="text" name="place" />
+            </div>
+            <div>
+              <input type="checkbox" />
+              <label>
+                I declare that the details mentioned above about my child are
+                true . I shall abide by the rule & refulations of the schoole
+              </label>
+            </div>
+            <div>
+              <button>Submit</button>
+            </div>
+          </div>
+        </div>
+      </form>
     </div>
   );
+};
 
-
+export default Adminission;
+{
+  /* <div className="g-row">
+          <div>
+            <label></label>
+            <input name = "" type =""/>
+          </div>
+        </div> */
 }
